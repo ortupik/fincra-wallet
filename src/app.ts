3d7444b233a4e35fff2d0ccbb126cd89f320f898
Chris Pchemut <chrispchemut@jumlaafrics.net>
2024-04-04 10:38:07 +0300

    added process env port

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import bodyParser from 'body-parser';
 import { walletRouter } from './walletController';
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
